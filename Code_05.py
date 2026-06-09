c2ae33bde1479d928477d5307434f9a1d8a18d17
Tushin-AS <abrarshariftushin003@outlook.com>
2026-06-09 10:39:23 +0600

    June 06th.

--- Code_05.py
+++ Code_05.py
@@ -1,1 +1,33 @@
 # Arithmetic & math
+import math
+
+a = 10
+b = 20
+c = 30
+d = 40
+e = 10
+
+
+a += 5
+b -= 5
+c *= 5
+d /= 5
+e **= 2
+remainder = a % 5
+
+x = 3.14
+y = -4
+z = 5
+
+result = round(x)
+Y = abs(y)
+expo = pow(4,3)
+max_res = max(a,b,c,d,e)
+min_res = min(a,b,c,d,e)
+
+print(math.pi)
+print(math.e)
+
+res1 = math.sqrt(e)
+res2 = math.ceil(x)
+res3 = math.floor(x)
---
+++ Code_07.py
@@ -0,0 +1,23 @@
+# Basic Calculator
+
+operator = input("Enter an operator (+ - * / %): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator == "/":
+    result = num1 / num2
+    print(round(result),4)
+elif operator == "%":
+    result = num1 % num2
+    print(result)
+else:
+    print("Invalid operator.")
